489ec9650270f355fe40a32d16dfd7e0af90a737
kim-hyein2 <kim_hyein@infonic.co.jp>
2021-06-13 15:24:34 +0900

    JS : Projects filtering

--- main.js
+++ main.js
@@ -23,28 +23,27 @@
         scrollIntoView(link );
     });
 
-
+    
     // Handle click on "contact me" button on home
     const contact = document.querySelector('.home__contact');
     contact.addEventListener('click', ()=> {
         scrollIntoView('#contact');
     });
-
+    
     function scrollIntoView(selector ) {
         const scrollTo = document.querySelector(selector );
         scrollTo.scrollIntoView({behavior: 'smooth'});
     }
-
+    
     // Make home slowly fade to transparent as the window scrolls down
     const home = document.querySelector('.home__container');
     const homeHeight = home.getBoundingClientRect().height;
     document.addEventListener('scroll', () => {
         home.style.opacity = 1 - window.scrollY / homeHeight;
     });
-
+    
     // Show "arrow up" button with scrolling down
     const arrowUp = document.querySelector('.arrow-up');
-    console.log(arrowUp);
     document.addEventListener('scroll', () => {
         if (window.scrollY > homeHeight / 2) {
             arrowUp.classList.add('visible');
@@ -52,10 +51,32 @@
             arrowUp.classList.remove('visible');
         }
     });
-
+    
     // Handle click on the "arrow up" button
     arrowUp.addEventListener('click', () => {
         scrollIntoView('#home');
     })
+    
+    // Projects
+    const workBtnContainer = document.querySelector('.work__categories');
+    const projectContainer = document.querySelector('.work__projects');
+    const projects = document.querySelectorAll('.project');
 
-}
+    workBtnContainer.addEventListener('click', (e) => {
+        const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+        if(filter == null) { 
+            return; 
+        }
+        projectContainer.classList.add('anim-out');
+        setTimeout(() => {
+            projects.forEach(project => {
+                if (filter === '*' || filter === project.dataset.type) {
+                    project.classList.remove('invisible');
+                } else {
+                    project.classList.add('invisible');
+                }
+            });
+            projectContainer.classList.remove('anim-out');
+        }, 300);
+    });
+} 
