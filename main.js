7840073cb7a9d2f6539b8cb578d70e8a50078765
kim-hyein2 <kim_hyein@infonic.co.jp>
2021-06-13 13:25:29 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -34,4 +34,12 @@
         const scrollTo = document.querySelector(selector );
         scrollTo.scrollIntoView({behavior: 'smooth'});
     }
+
+    // Make home slowly fade to transparent as the window scrolls down
+    const home = document.querySelector('.home__container');
+    console.log(home);
+    const homeHeight = home.getBoundingClientRect().height;
+    document.addEventListener('scroll', () => {
+        home.style.opacity = 1 - window.scrollY / homeHeight;
+    });
 }
