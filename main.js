48c62196eea50969c02967a76a849557f4abb80e
kim-hyein2 <kim_hyein@infonic.co.jp>
2021-06-26 11:26:17 +0900

    modify toggle btn

--- main.js
+++ main.js
@@ -3,27 +3,32 @@
 {
     //Make navbar transparent when it is on the top
     const navbar = document.querySelector('#navbar');
-    const navbarHeight = navbar.getBoundingClientRect().height;
     document.addEventListener('scroll', () => {
-        if (window.scrollY > navbarHeight) {
+        if (window.scrollY > homeHeight) {
             navbar.classList.add('navbar--dark');
         } else {
             navbar.classList.remove('navbar--dark');
         }
     });
-
+    
     // Handle scrolling when tapping on the navbar menu
     const navbarMenu = document.querySelector('.navbar__menu');
     navbarMenu.addEventListener('click', (event) => {
         const target = event.target;
         const link = target.dataset.link;
-        if(link == null) { 
-            return; //조기리턴
+        if (link == null) {
+            return;
         }
-        scrollIntoView(link );
+        navbarMenu.classList.remove('open');
+        scrollIntoView(link);
+    });
+    
+    // Navbar toggle buttton for small screen
+    const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+    navbarToggleBtn.addEventListener('click', () => {
+        navbarMenu.classList.toggle('open');
     });
 
-    
     // Handle click on "contact me" button on home
     const contact = document.querySelector('.home__contact');
     contact.addEventListener('click', ()=> {
@@ -68,7 +73,7 @@
             return; 
         }
 
-        // Remove selection from the previous item and select the new one
+    // Remove selection from the previous item and select the new one
         const active = document.querySelector('.category__btn.selected');
         active.classList.remove('selected');
         const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
